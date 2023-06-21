01bc8d3b5c252901a6bac1d47386cb498c2fd752
Joseph <joseph161216@gmail.com>
2023-06-20 21:54:05 -0700

    Add passport

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
+import passport from "passport";
+import bodyParser from "body-parser";
 
-dotenv.config();
+import userController from "./src/controllers/user.controller";
 
 const app: Express = express();
+app.use(bodyParser.json());
+app.use(passport.initialize());
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server!!");
 });
+
+// Routes
+app.use("/", userController);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
